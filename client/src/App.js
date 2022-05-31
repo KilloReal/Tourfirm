a2ce08f63873d9d8cc688868e81d9c25aaac1a87
ultram4rine <ultramarine730@gmail.com>
2022-05-31 16:13:53 +0000

    fix react

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 import CreateBook from './components/CreateBook';
@@ -10,14 +10,14 @@ import UpdateBookInfo from './components/UpdateBookInfo';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <div>
           <Route exact path='/' component={ShowBookList} />
           <Route path='/create-book' component={CreateBook} />
           <Route path='/edit-book/:id' component={UpdateBookInfo} />
           <Route path='/show-book/:id' component={ShowBookDetails} />
         </div>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
