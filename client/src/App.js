59673cede8518611bfa5c2c60b2c24cc9d7cee70
ultram4rine <ultramarine730@gmail.com>
2023-04-16 16:47:25 +0000

    some kind of auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
+import { history } from './helpers';
+
+import { Login } from './components/Login'
+import { Register } from './components/Register'
+
 import CreateTour from './components/CreateTour';
 import ShowTourList from './components/ShowTourList';
 import ShowTourDetails from './components/ShowTourDetails';
@@ -10,8 +15,10 @@ import UpdateTourInfo from './components/UpdateTourInfo';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <div>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
           <Route exact path='/' component={ShowTourList} />
           <Route path='/create-tour' component={CreateTour} />
           <Route path='/edit-tour/:id' component={UpdateTourInfo} />
